ecaebce822a2aa00c1abee8b05106949e8073764
Pablo Naveira <pablonaveiralopez@gmail.com>
2021-05-21 13:05:08 +0200

    fixing routes

--- js/main.js
+++ js/main.js
@@ -6,6 +6,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const ham = document.getElementById("ham");
   const enlacesResponsive = document.getElementById("enlaces-responsive");
   const close = document.getElementById("close");
+  const link = document.getElementsByClassName("link");
+
+  for (var i = 0; i < link.length; i++) {
+    link[i].onclick = () => {
+      enlacesResponsive.classList.toggle("active");
+    };
+  }
 
   ham.onclick = () => {
     enlacesResponsive.classList.toggle("active");
