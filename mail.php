4040fabb36c447cd93059a6125be47209ea1db19
Pablo Naveira <pablonaveiralopez@gmail.com>
2021-05-25 12:29:30 +0200

    fixed mail

--- mail.php
+++ mail.php
@@ -22,3 +22,21 @@ if (isset($_POST['url']) && $_POST['url'] == '') {
     mail($youremail, 'Mensaje desde la web', $body, $headers);
 
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Envio formulario</title>
+</head>
+
+<body>
+    <h2>Enviado correctamente</h2>
+</body>
+
+</html>
